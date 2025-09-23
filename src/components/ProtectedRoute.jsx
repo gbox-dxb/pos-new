c1a09b9a1d76dc3df47d4a1341fc2ca0a8f6375c
mohammad <awais3227443@gmail.com>
2025-09-23 22:28:43 +0400

    route fixes

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,9 +3,13 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { useAccessControl } from '@/contexts/AccessControlContext';
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated } = useAccessControl();
+  const { isAuthenticated, loading } = useAccessControl();
   const location = useLocation();
-
+  
+  if (loading) {
+    return <div>Loading...</div>; // Or a spinner
+  }
+  
   if (!isAuthenticated) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
