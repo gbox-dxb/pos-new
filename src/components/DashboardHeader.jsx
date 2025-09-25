1ba5a6f9fc672b2e0d6a38676fa6d2257984a012
mohammad <awais3227443@gmail.com>
2025-09-25 17:56:31 +0400

    loading

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -78,8 +78,21 @@ const DashboardHeader = ({
         <div className="flex flex-wrap items-center gap-2 mt-4 md:mt-0 justify-between">
           {permissions.viewRevenue && (
             <Card className="p-2 px-4 flex items-center gap-2">
-              <DollarSign className="h-5 w-5 text-green-500" />
-              <span className="font-bold text-foreground">{revenueString}</span>
+              {loading ? (
+                <>
+                  <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+                       fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+                </>
+              ) : (
+                <>
+                  <DollarSign className="h-5 w-5 text-green-500" />
+                  <span className="font-bold text-foreground">{revenueString}</span>
+                </>
+              )}
             </Card>
           )}
           {permissions.addStore && (
