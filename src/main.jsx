af44d92d5966adddd86a90ec43d0eadac183237f
mohammad <awais3227443@gmail.com>
2025-09-27 21:57:37 +0400

    disabled page analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from '@/App';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import '@/styles/design-system.css';
 import '@/styles/index.css';
-import { inject } from '@vercel/analytics';
 
-inject();
+// import { inject } from '@vercel/analytics';
+// inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
