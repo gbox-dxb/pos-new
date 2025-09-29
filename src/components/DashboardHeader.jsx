71fafd43cadd4cdf7f6434b3d426a4a5540e6153
mohammad <awais3227443@gmail.com>
2025-09-29 16:00:32 +0400

    fix msg

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -60,9 +60,9 @@ const DashboardHeader = ({
     if (hour >= 5 && hour < 12) {
       return "☀️ Good Morning";
     } else if (hour >= 12 && hour < 17) {
-      return "Good Afternoon";
+      return "☀️ Good Afternoon";
     } else if (hour >= 17 && hour < 21) {
-      return "Good Evening";
+      return "🌙 Good Evening";
     } else {
       return "🌙 Good Night";
     }
